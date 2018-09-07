0b790dac0b8a3d553911cdc43be1e68480a3c61a
Zuhair Haroon Khan <zk222ac@gmail.com>
2018-09-08 00:46:41 +0200

    Ship is ready for sink

--- TCPChatClient/Program.cs
+++ TCPChatClient/Program.cs
@@ -1,16 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TCPChatClient
 {
-    class Program
+   class Program
     {
+        private static TcpClient _clientSocket = null;
+        private static Stream _nstream = null;
+        private static StreamWriter _sWriter = null;
+        private static StreamReader _sReader = null;
         static void Main(string[] args)
         {
-            Console.WriteLine("write your answer");
+            try
+            {
+                // Create 3-way Handshake 
+                
+                // Step no : 1
+                //TCP establish connection via its socket through request to server
+
+                // Step no: 3 
+                // when TCP connection is established then client send (bytes of data) to server
+                using (_clientSocket = new TcpClient("127.0.0.1", 6789))
+                {
+                    using (_nstream = _clientSocket.GetStream())
+                    {
+                        // Data will be flushed from the buffer to the stream after each write operation
+                        _sWriter = new StreamWriter(_nstream) {AutoFlush = true};
+                        Console.WriteLine("Client ready to send bytes of data to server...");
+                        Console.WriteLine("Kindly write your message here ");
+                        string clientMsg = Console.ReadLine();
+                        // client ready to send (bytes of data) which has collected through user input
+                        _sWriter.WriteLine(clientMsg);
+
+                        // Step no: 6 ........................................
+                        // Client recieved the (modified server Message) sent back by server to client 
+                        // perform write operation 
+                        _sReader = new StreamReader(_nstream);
+                        string rdMsgFromServer = _sReader.ReadLine();
+                        if (rdMsgFromServer != null)
+                        {
+                            Console.WriteLine(".....................................................");
+                            Console.WriteLine("Client recieved Message from Server:" + rdMsgFromServer);
+                            Console.WriteLine(".....................................................");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Client recieved null message from Server ");
+                        }
+                    }
+                }
+
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadKey();
+            }
         }
+
     }
+
 }
